1bc4c0bb7d461e9ae017040c0c71045f8068d5ad
ermc33 <edwin.mercado@upr.edu>
2017-02-09 16:54:45 -0800

    VideoRecording.py
    Code for recording a video file using opencv

---
+++ VideoRecording.py
@@ -0,0 +1,19 @@
+
+import numpy as np
+import cv2
+
+#Video capture object. (0)-> number of camera device
+camera = cv2.VideoCapture(0)
+#Define video object. Second parameter is the video codec
+#Third parameter is the FPS of the video and last one is the FrameSize
+video = cv2.VideoWriter('video10.avi', -1,10, (640,480));
+
+while True:
+    frame,imag = camera.read()
+    video.write(imag)
+    cv2.imshow("webcam", imag)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+#cv2.destroyAllWindows()
+video.release()
