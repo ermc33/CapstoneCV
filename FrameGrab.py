f273f48f8ed39400646747c1ee845fa3171a39ce
ermc33 <edwin.mercado@upr.edu>
2017-03-24 12:15:52 -0400

    Add files via upload
    NewFrame grab

--- FrameGrab.py
+++ FrameGrab.py
@@ -23,20 +23,21 @@ Created on Tue Feb 21 16:30:24 2017
 import cv2
 import numpy as np
 import time
+
 counter = 0
  
 cap = cv2.VideoCapture(0)
-
-while True:
+#while True = multiples fotos
+while counter < 2:
     
     ret, frame  = cap.read()
-    time.sleep(2)
-    cv2.imwrite("frame%d.jpg" % counter,frame)
+    time.sleep(3)
+    cv2.imwrite("path%d.jpg" % counter,frame)
     counter +=1
     print counter
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    
+execfile('Path.py')    
     
     
 cap.release()
